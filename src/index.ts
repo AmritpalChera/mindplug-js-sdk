114bd0f331cea8fed36d8f46128fed5f6f6d4210
AmritpalChera <cheraamritpal@gmail.com>
2023-10-19 19:51:00 -0400

    add publish tweet

--- src/index.ts
+++ src/index.ts
@@ -166,4 +166,8 @@ export default class Mindplug {
   async labelText(text: string, amount?: number) {
     return this.mindplug.post('/smart/label', {text, number: amount}).then((res: any) => res.data).catch((err: any) => err.response.data);
   }
+
+  async generateTweet(text: string, tone?: string) {
+    return this.mindplug.post('/smart/generateTweet', {text, tone}).then((res: any) => res.data).catch((err: any) => err.response.data);
+  }
 }
